3f366a88434c0266025e35c069db91980601adc3
A-t-s-u-s-h-i <sasakisasasakiatsushi@yahoo.co.jp>
2021-06-13 19:37:25 +0900

    coded /register-and-broadcast-node and /register-node

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const { v1: uuid } = require('uuid');
 const port = process.argv[2];
+const rp = require('request-promise');
 
 const nodeAddress = uuid().split('-').join('');
 const bitcoin = new Blockchain();
@@ -44,11 +45,47 @@ app.get('/mine', function(req, res) {
 // register a node and broadcast it the network.
 app.post('/register-and-broadcast-node', function(req, res) {
     const newNodeUrl = req.body.newNodeUrl;
+    if (bitcoin.networkNodes.indexOf(newNodeUul) == -1) {
+        bitcoin.networkNodes.push(newNodeUrl);
+    }
+
+    const regNodesPromises = [];
+    bitcoin.networkNodes.forEach(networkNodeUrl => {
+        const requestOptions = {
+            uri: nwtworkNodeUrl + '/register-node',
+            method: 'POST',
+            body: { newNodeUrl: newNodeUrl},
+            json: true
+        };
+
+        regNodesPromises.push(rp(requestOptions));
+    });
+
+    Promise.all(regNodesPromises)
+    .then(data => {
+        const bulkRegisterOptions = {
+            uri: newNodeUrl + 'register-nodes-bulk',
+            method: 'Post',
+            body: { allNetworkNodes: [ ...bitcoin.networkNodes, bitcoin.currentNodeUrl ] },
+            json: true
+        };
+
+        return rp(bulkRegisterOptions);
+    })
+    .then(data => {
+        res.json({ note: 'New node registerd with network successfully.' })
+    });
 });
 
 // register a node with the network
 app.post('/register-node', function(req, res) {
-
+    const newNodeUrl = req.body.newNodeUrl;
+    const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1;
+    const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl;
+    if (nodeNotAlreadyPresent && notCurrentNode) {
+        bitcoin.networkNodes.push(newNodeUrl);
+    }
+    res.json({ note: 'New node registerd successfuly.' });
 });
 
 // register multiple nodes at once
